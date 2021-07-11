d7a409a1828a895330500c0ffbe958afedb7e1a1
nooRok <>
2021-07-11 20:59:58 +0900

    Implement multiplication to Value

--- icr2_3dto3do/value.py
+++ icr2_3dto3do/value.py
@@ -3,3 +3,10 @@ class Value(list):
     def __init__(self, *args, **kwargs):
         super().__init__(*args)
         self.attrs = {k.lower() if len(k) == 1 else k: v for k, v in kwargs.items()}
+
+    def __mul__(self, other):
+        assert isinstance(other, float)
+        return self.__class__([int(x * other) for x in self], **self.attrs)
+
+    def __imul__(self, other):
+        self[:] = self * other
