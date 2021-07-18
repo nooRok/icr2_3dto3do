f6112f75cc782146de9e77731ff9c796042d0ca5
nooRok <>
2021-07-18 21:05:08 +0900

    ver.0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='icr2_3dto3do',
-    version='0.1.2rc2',
+    version='0.1.2',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/nooRok/icr2_3dto3do',
     license='MIT',
